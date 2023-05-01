7f48faf8302bf1636bd50218a7a9893ef050b1bb
martinpericht <martinpericht@centrum.sk>
2023-05-01 20:47:46 +0200

    uprava 1.5.2023

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // nav menu
   const menus = document.querySelectorAll('.side-menu');
   M.Sidenav.init(menus, {edge: 'left'});
-  // add recipe form
+  // add homeless
   const forms = document.querySelectorAll('.side-form');
   M.Sidenav.init(forms, {edge: 'right'});
 });
